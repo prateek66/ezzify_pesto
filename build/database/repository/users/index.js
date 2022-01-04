c4d1d4fc5c22ca9209fd4e6652e4eaad61381cec
Harshit <harshitsinghchouhan2018@gmail.com>
2022-01-05 00:15:05 +0530

    Updates

--- build/database/repository/users/index.js
+++ build/database/repository/users/index.js
@@ -1,23 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -25,7 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.UsersProps = exports.UsersDto = exports.verifyProps = exports.VerifyDto = exports.UsersDB = exports.User = void 0;
 var users_model_1 = require("./users.model");
 Object.defineProperty(exports, "User", { enumerable: true, get: function () { return __importDefault(users_model_1).default; } });
-exports.UsersDB = __importStar(require("./users.db"));
+var users_db_1 = require("./users.db");
+Object.defineProperty(exports, "UsersDB", { enumerable: true, get: function () { return users_db_1.UsersDB; } });
 var verify_dto_1 = require("./verify/verify.dto");
 Object.defineProperty(exports, "VerifyDto", { enumerable: true, get: function () { return verify_dto_1.VerifyDto; } });
 var verify_props_1 = require("./verify/verify.props");

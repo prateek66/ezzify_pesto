95c949a0440d47261442e6be2920f65690db0a47
Medusa <kawthekar56@gmail.com>
2022-01-09 00:36:07 +0530

    Updates

---
+++ build/database/repository/vendors/updateVendor/updateVendor.dto.d.ts
@@ -0,0 +1,11 @@
+export declare class UpdateUsersDto {
+    firstName: string;
+    lastName: string;
+    mobileNumber: string;
+    profileImage: string;
+    city: string;
+    state: string;
+    isActive: string;
+    isEmaiVerified: string;
+    roles: string;
+}

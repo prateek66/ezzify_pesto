a7f2fe021b24e4632dd4ef1b0112459b87a85f2b
Medusa <kawthekar56@gmail.com>
2022-01-16 20:45:20 +0530

    Updates

--- build/interfaces/index.d.ts
+++ build/interfaces/index.d.ts
@@ -4,3 +4,4 @@ export { UsersInterface } from "./users/users.interface";
 export { VerifyInterface } from "./users/verify.interface";
 export { decodedJWT } from "./auth/jwt.interface";
 export { ServicesInterface } from "./services/services.interface";
+export { BookingInterface } from "./bookings/booking.interface";
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -4,4 +4,5 @@ export { UsersInterface } from "./users/users.interface";
 export { VerifyInterface } from "./users/verify.interface";
 export { decodedJWT } from "./auth/jwt.interface";
 export { ServicesInterface } from "./services/services.interface";
+export { BookingInterface } from "./bookings/booking.interface";
 

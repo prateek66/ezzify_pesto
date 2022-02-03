a236a6038a71c8b0c6002d45f73cfa2757e1764d
Harshit <harshitsinghchouhan2018@gmail.com>
2022-02-03 10:00:24 +0530

    Updates

--- build/config/config.d.ts
+++ build/config/config.d.ts
@@ -3,8 +3,6 @@ declare const config: {
     MONGO_PASSWORD: string;
     ENCRYPTION_KEY: string;
     ENVIRONMENT: string;
-    SENDGRID_API_KEY: string;
-    SENDGRID_SENDER_EMAIL: string;
     ACCESS_KEY_S3: string;
     SECRET_KEY_S3: string;
     AWS_REGION: string;
--- build/config/config.js
+++ build/config/config.js
@@ -4,12 +4,10 @@ var config = {
     MONGO_CONNECTION_STRING: "mongodb+srv://admin:<PASSWORD>@cluster0.jgaoo.mongodb.net/Ezzify_Zindagi?retryWrites=true&w=majority",
     MONGO_PASSWORD: "root",
     ENCRYPTION_KEY: "EZZIFY-PESTO",
-    ENVIRONMENT: "DEV",
-    SENDGRID_API_KEY: "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",
-    SENDGRID_SENDER_EMAIL: "kawthekar56@gmail.com",
+    ENVIRONMENT: "PROD",
     ACCESS_KEY_S3: "AKIARZJEKPRNBYIF5LMJ",
     SECRET_KEY_S3: "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",
     AWS_REGION: "ap-south-1",
-    AWS_BUCKET: "ezzifypesto"
+    AWS_BUCKET: "ezzifypesto",
 };
 exports.default = config;

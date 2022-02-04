90601a7e532e11389071b5978ec02b484d0462fe
Harshit <harshitsinghchouhan2018@gmail.com>
2022-02-05 00:10:43 +0530

    Updates

--- build/config/config.js
+++ build/config/config.js
@@ -4,7 +4,7 @@ var config = {
     MONGO_CONNECTION_STRING: "mongodb+srv://admin:<PASSWORD>@cluster0.jgaoo.mongodb.net/Ezzify_Zindagi?retryWrites=true&w=majority",
     MONGO_PASSWORD: "root",
     ENCRYPTION_KEY: "EZZIFY-PESTO",
-    ENVIRONMENT: "PROD",
+    ENVIRONMENT: "DEV",
     ACCESS_KEY_S3: "AKIARZJEKPRNBYIF5LMJ",
     SECRET_KEY_S3: "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",
     AWS_REGION: "ap-south-1",
